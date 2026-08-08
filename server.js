7537e82d84b2a8e53c3cac9e8662f59c23ba1dda
sky_software_solution <skysoftware2020@gmail.com>
2026-08-08 09:49:58 +0530

    Prepare backend for Vercel

--- server.js
+++ server.js
@@ -17,12 +17,21 @@ app.get("/", (req, res) => {
   res.send("Student Login API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
+/* const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   
-});
+}); */
+if (require.main === module) {
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
 
 /* const express = require("express");
 const mongoose = require("mongoose");
